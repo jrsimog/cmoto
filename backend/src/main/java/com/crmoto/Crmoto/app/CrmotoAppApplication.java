43f7668ba4ad71ca5c8a34732bbbc4461a99aa24
Jose <jose@recemed.com>
2025-02-02 16:36:42 -0300

    initial commit :D

---
+++ backend/src/main/java/com/crmoto/Crmoto/app/CrmotoAppApplication.java
@@ -0,0 +1,13 @@
+package com.crmoto.Crmoto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmotoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmotoAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/crmoto/Crmoto/app/CrmotoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmoto.Crmoto.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmotoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
